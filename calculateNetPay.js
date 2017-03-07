c35a5268288372bc0dc8631075801a4972c096bb
Annie Sing <anniesing@annies-air.gateway.sonic.net>
2017-03-06 19:10:46 -0800

    Add more payment details and ability to open printable version

--- calculateNetPay.js
+++ calculateNetPay.js
@@ -1,25 +1,79 @@
+const grosspay = (hours, hourlyWage) => {
+  return Number(hours * hourlyWage);
+};
+if (grosspay(8, 10) !== 80) {
+  throw new Error('you wrong');
+}
+
+const fica = grossPay => Number(grossPay * .062);
+if (fica(960).toFixed(2) !== '59.52') {
+  throw new Error('you wrong');
+}
+
+const medicare = grossPay => Number(grossPay * .0145);
+if (medicare(960).toFixed(2) !== '13.92') {
+  throw new Error('you wrong');
+}
+
 const netPay = (hours, hourlyWage, withheld) => {
-	const grossPay = hours * hourlyWage;
-	const netPay = (grossPay - withheld) - (grossPay * .062) - (grossPay * .0145);
-	const roundedNet = netPay.toFixed(3);
-	return Number(roundedNet);
+  const grossPay = grosspay(hours, hourlyWage);
+  const netPay = (grossPay - withheld) - fica(grossPay) - medicare(grossPay);
+  return Number(netPay);
+};
+if (netPay(8, 10, 10).toFixed(2) !== '63.88') {
+  debugger;
+  throw new Error('you wrong');
 }
 
-if(netPay(8, 10, 10) !== 63.880) {
-	throw new Error('you wrong');
-}
+const update = (id, text) => {
+  const $div = document.createElement('div');
+  $div.id = id;
+  $div.appendChild(document.createTextNode(text));
 
-const el = document.getElementById('calculate');
-el.addEventListener('click', () => {
-	const hours = document.getElementById('hours').value;
-	const hourlyWage = document.getElementById('wages').value;
-	const withheld = document.getElementById('withheld').value;
-	const netPays = netPay(hours, hourlyWage, withheld);
-	
-	const $div = document.createElement('div');
-	$div.id = 'net-pay';
-	$div.appendChild(document.createTextNode(`Net Pay: $ ${netPays}`));
-	
-	const $oldDiv = document.getElementById('net-pay');
-	document.body.replaceChild($div, $oldDiv);
+  const $oldDiv = document.getElementById(id);
+  document.getElementById('pay-details').replaceChild($div, $oldDiv);
+};
+
+const valueFor = (selector) => {
+  return document.getElementById(selector).value
+};
+
+const calculateAndShowPayDetails = () => {
+  const hours = valueFor('hours');
+  const hourlyWage = valueFor('wages');
+  const withheld = valueFor('withheld');
+  const netPays = netPay(hours, hourlyWage, withheld);
+  const grossPay = grosspay(hours, hourlyWage);
+
+  update('name', `Employee Name: ${valueFor('employee-name')}`);
+  update('ssn', `Employee SSN: ${valueFor('employee-ssn')}`);
+  update('pay-period', `Pay Period: ${valueFor('employee-pay-period')}`);
+  update('pay-date', `Pay Date: ${valueFor('employee-pay-date')}`);
+  update('total-hours', `Total Hours Worked: ${valueFor('hours')}`);
+  update('hourly-wage', `Hourly Wage: ${valueFor('wages')}`);
+
+  update('net-pay', `Net Pay: $ ${netPays.toFixed(2)}`);
+  update('gross-pay', `Gross Pay: $ ${grossPay.toFixed(2)}`);
+  update('fed-witholding', `Federal Withholding: $ ${withheld}`);
+  update('fica', `FICA: $ ${fica(grossPay).toFixed(2)}`);
+  update('medicare', `Medicare: $ ${medicare(grossPay).toFixed(2)}`);
+};
+
+const printDetails = () => {
+  const payDetails = document.getElementById('pay-details').outerHTML;
+  const printWindow = window.open('', '', 'height=800,width=1400');
+  printWindow.document.write('<html><head><title>Pay Details</title>');
+  printWindow.document.write('</head><body >');
+  printWindow.document.write(payDetails);
+  printWindow.document.write('</body></html>');
+  printWindow.document.close();
+  // printWindow.print();
+};
+
+document.getElementById('calculate').addEventListener('click', () => {
+  calculateAndShowPayDetails();
+});
+
+document.getElementById('print').addEventListener('click', () => {
+  printDetails();
 });
